70b2601162212e58dfdb2636f694ceee886c742b
stanislavtvs <kirichenkostanislav2005@gmail.com>
2023-01-13 01:45:02 +0600

    Добавил Одноклассников

--- web/scripts.js
+++ web/scripts.js
@@ -1,6 +1,7 @@
 function allcloseblock() {
     document.getElementById("predmet_block").style.display = "none";
     document.getElementById("block_schedulespredmet").style.display = "none";
+    document.getElementById("block_classmates").style.display = "none";
 }
 function predmeticlick() {
     allcloseblock();
@@ -12,11 +13,20 @@ function Schedulesclick() {
     document.getElementById("block_schedulespredmet").style.display = "block";
     detectschedules();
 }
+function Classmatesclick() {
+    document.getElementById("block_classmates_content").innerHTML = "";
+    allcloseblock();
+    document.getElementById("block_classmates").style.display = "block";
+    detectuser_py();
+}
 async function detectpredmet(login) {
     await eel.detectpredmet(login);
 }
 async function detectschedules() {
     await eel.detectschedules();
+}
+async function detectuser_py() {
+    await eel.detectuser_py();
 }
 eel.expose(detectpredmetout);
 function detectpredmetout(text) {
@@ -44,5 +54,13 @@ function detectschedulesout(text) {
 
         block = '<div class="block_navigator_schedules"><div class="block_navigator_schedules_ponel_center_end"><p class="block_navigator_ponel_text" style="width: 100%;margin-left: 0px;border-radius:25px;">'+element+'</p></div><div class="ubd_butt_schedulestitle" id='+ide+' onclick="ubd_butt_schedulestitle(this)"><h3>✏️</h3></div>'+textp+'</div>'
         document.getElementById("block_schedulespredmet_content").innerHTML += block;
+    }
+}
+eel.expose(detectuser_js);
+function detectuser_js(text) {
+    for (let index = 0; index < text.length; index++) {
+        textp = "<div class='classmates_list_block'><img src='"+text[index]['ava']+"' alt='ava'><p>"+text[index]['name']+"</p></div>";
+        document.getElementById("block_classmates_content").innerHTML += textp;
+        //console.log(text[index])
     }
 }
