ce62dd0230daf2816b0fc0ee6cb363fc4c65b6af
stanislavtvs <kirichenkostanislav2005@gmail.com>
2023-01-13 00:32:46 +0600

    Update scripts.js

--- web/scripts.js
+++ web/scripts.js
@@ -1,4 +1,17 @@
 function predmeticlick() {
     document.getElementById("predmet_block").style.display = "block";
-    //detectpredmet(login);
+    detectpredmet("stanislavkirichenko");
+}
+async function detectpredmet(login) {
+    await eel.detectpredmet(login);
+}
+eel.expose(detectpredmetout);
+function detectpredmetout(text) {
+    document.getElementById("block_predmet_content").innerHTML = "";
+    for (let index = 0; index < text.length; index++) {
+        var element = text[index]['title'];
+        var elementimg = text[index]['title'].replace(/ /g,'');
+        block = '<div class="block_navigator" style="background-image: url(img/pimg/'+elementimg+'.jpg);" img="img/pimg/'+elementimg+'.jpg" title="'+element+'" idpost="'+text[index]['id']+'" onclick="predmet_ponel_click(this)"><div class="block_navigator_ponel_center_end"><p class="block_navigator_ponel_text">'+element+'</p></div></div>'
+        document.getElementById("block_predmet_content").innerHTML += block;
+    }
 }
